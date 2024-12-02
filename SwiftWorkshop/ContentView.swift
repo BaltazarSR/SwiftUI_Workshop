02daa87ea6626c46e0100c7506d05bb58c61181c
Baltazar Servin <thebaltazarsr@gmail.com>
2024-12-02 12:00:52 -0600

    Initial Commit

---
+++ SwiftWorkshop/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SwiftWorkshop
+//
+//  Created by Baltazar Servin on 02/12/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SwiftWorkshop/SwiftWorkshopApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftWorkshopApp.swift
+//  SwiftWorkshop
+//
+//  Created by Baltazar Servin on 02/12/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftWorkshopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
