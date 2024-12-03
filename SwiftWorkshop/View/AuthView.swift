368607a28580c82d1e386bc047c87380e7079cca
Baltazar Servin <thebaltazarsr@gmail.com>
2024-12-03 13:18:43 -0600

    Authentication UI & Login

--- SwiftWorkshop/View/AuthView.swift
+++ SwiftWorkshop/View/AuthView.swift
@@ -8,28 +8,67 @@
 import SwiftUI
 
 struct AuthView: View {
-    @State var email: String = ""
-    @State var password: String = ""
+    @State var authViewModel = AuthViewModel()
     @FocusState private var focus: String?
     
     var body: some View {
         VStack {
-            TextField("Email", text: $email)
-                .padding(16)
-                .background(Color(uiColor:
-                        .secondarySystemBackground),
-                            in: RoundedRectangle(cornerRadius: 16))
-                .border(focus == "emailField" ? Color.black : .clear)
-                .focused($focus, equals:"emailField")
-                .textContentType(.emailAddress)
-                .keyboardType(.emailAddress)
-            
-            SecureField("Password", text: $password)
+            TextField("Email", text: $authViewModel.email)
                 .textFieldStyle(RoundedTextFieldStyle())
+//                .padding(16)
+//                .background(Color(uiColor:
+//                        .secondarySystemBackground),
+//                            in: RoundedRectangle(cornerRadius: 16))
+//                .border(focus == "emailField" ? Color.black : .clear)
+//                .focused($focus, equals:"emailField")
+//                .textContentType(.emailAddress)
+//                .keyboardType(.emailAddress)
+                
+            
+            SecureField("Password", text: $authViewModel.password)
+                .textFieldStyle(RoundedTextFieldStyle())
+            ViewThatFits {
+                HStack{
+                    registerButton()
+                    loginButton()
+                }
+                .disabled(self.authViewModel.email.isEmpty || self.authViewModel.password.isEmpty)
+                
+                VStack{
+                    registerButton()
+                    loginButton()
+                }
+                .disabled(self.authViewModel.email.isEmpty || self.authViewModel.password.isEmpty)
+            }
+            
         }
         
         .padding(16)
         
+    }
+    
+    @ViewBuilder private func registerButton() -> some View {
+        Button {
+            Task {
+                await self.authViewModel.registerUser()
+            }
+        } label: {
+            Text("Register")
+                .frame(maxWidth: .infinity)
+        }
+        .buttonStyle(.bordered)
+    }
+    
+    @ViewBuilder private func loginButton() -> some View {
+        Button {
+            Task {
+                await self.authViewModel.loginWithEmailPassword()
+            }
+        } label: {
+            Text("Login")
+                .frame(maxWidth: .infinity)
+        }
+        .buttonStyle(.borderedProminent)
     }
 }
 
